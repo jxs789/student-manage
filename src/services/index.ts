f8ee45b8990cc91971ca321c6a14adcc330672ac
hanna0908 <h5139706n@163.com>
2019-08-28 21:34:41 +0800

    upDate

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,8 @@
-import request from '../utils/request';
+// import request from '../utils/request';
 
-export function getRegister(params:Object): Promise<Object>{
-    console.log(params,'222')
-    return request.get('/emstu/teacher/register');
-}
-// {username:params.inp,password:params.pwd}
+// export function getRegister(params:Object): Promise<Object>{
+//     console.log(params,'222')
+//     return request.get('/emstu/teacher/register');
+// }
+// // {username:params.inp,password:params.pwd}
+export * from "./user"
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,29 +1,27 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// // create an axios instance
-// const service = axios.create({
-// //   baseURL: /h5.chelun.com/.test(window.location.origin)? 'https://baojia.chelun.com/':'http://baojia-test.chelun.com/',
-//   baseURL: /h5.chelun.com/.test(window.location.origin)? 'http://148.70.121.59:9001':'http://148.70.121.59:9001',  
-//   withCredentials: true, // 跨域请求时发送 cookies
-//   timeout: 5000 // request timeout
-// })
+// create an axios instance
+const service = axios.create({
+    baseURL: '',
+    timeout: 5000 // request timeout
+})
 
-// // request interceptor
-// service.interceptors.request.use(
-//   config => {
-//     return config
-//   },
-//   error => {
-//     return Promise.reject(error)
-//   }
-// )
+// request interceptor
+service.interceptors.request.use(
+    config => {
+        return config
+    },
+    error => {
+        return Promise.reject(error)
+    }
+)
 
-// // response interceptor
-// service.interceptors.response.use(
-//   response => response.data,
-//   error => {
-//     return Promise.reject(error)
-//   }
-// )
+// response interceptor
+service.interceptors.response.use(
+    response => response.data,
+    error => {
+        return Promise.reject(error)
+    }
+)
 
-// export default service
+export default service
